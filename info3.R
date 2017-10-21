5617e3ba0584d771efc947b60a1baa0cc6abb85c
drickless <drickless1@gmail.com>
2017-10-21 13:12:54 -0400

    add visualizations for Q1

--- info3.R
+++ info3.R
@@ -1,14 +1,20 @@
 library(tidyverse)
-library(ggplot2)
 
-sgeo <- read.csv("/Volumes/SANDISK/CoastSurvey/sgeoTable.csv")
+sgeo <- read.csv("~/Desktop/sgeoTable.csv")
+
 a <- ggplot(data = sgeo)
 
 a +
-  geom_bar(mapping = aes(x = Hincome))
+  geom_bar(mapping = aes(x = as.factor(Hincome))) +
+    scale_x_discrete(breaks = c(1:7), 
+                     labels = c("<25K","25-35K","35-50K","50-75K","75-100K","100-150K",">150K"))
 a +
-  geom_bar(mapping = aes(x = Ethnicity))
+  geom_bar(mapping = aes(x = as.factor(Ethnicity))) +
+    scale_x_discrete(breaks = c(1:8), 
+                     labels = c("White","Hispanic","Black","Asian","Native American",
+                                "MidEast/N.Africa","Pacific Islander","Other"))
 a +
-  geom_bar(mapping = aes(x = Education))
-a +
-  geom_bar(mapping = aes(x = FldRisk))
+  geom_bar(mapping = aes(x = as.factor(Education))) +
+  scale_x_discrete(breaks = c(1:7),
+                   labels = c("Did not finish HS","HS/GED","Some college","Assoc","Bachelors","Some grad","Grad degree"))
+
